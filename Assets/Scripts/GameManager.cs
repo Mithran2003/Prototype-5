8d2aa0c96199a423a523c52f834ab043140ce537
Mithran2003 <mithransaini5@gmail.com>
2026-05-31 11:24:19 +0530

    added spwaner to game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,15 +6,26 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> targets ;
+    [SerializeField]
+    private float spwanDelay ;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(spwanObjects());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator spwanObjects()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spwanDelay);
+            int index = Random.Range(0,targets.Count);
+            Instantiate(targets[index]);
+        }
     }
 }
