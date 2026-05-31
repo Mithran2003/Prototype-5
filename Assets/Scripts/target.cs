d71c269b3b6d24037a72c232091bb76f32ef52cb
Mithran2003 <mithransaini5@gmail.com>
2026-05-31 10:33:02 +0530

    added random force and rotation to object

--- Assets/Scripts/target.cs
+++ Assets/Scripts/target.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class target : MonoBehaviour
 {
+    private Rigidbody targetRb;
+    private Vector3 spawnPoint ;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(Vector3.up * Random.Range(10,16),ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10,10),Random.Range(-10,10),Random.Range(-10,10),ForceMode.Impulse);
+        spawnPoint.x = Random.Range(-4.75f,4.75f);
+        spawnPoint.y = -1.75f;
+        transform.position = spawnPoint;
     }
 
     // Update is called once per frame
